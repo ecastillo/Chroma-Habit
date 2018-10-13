e1d097b9147a1ec5bbf3f48d286f2207086cebdc
Eric Castillo <ecastillo11@gmail.com>
2018-10-12 22:33:47 -0500

    still trying to clean up repo

--- Chroma Habit/AppDelegate.swift
+++ Chroma Habit/AppDelegate.swift
@@ -14,7 +14,7 @@ let recordModifiedOnWatchNotificationID = "com.chromahabit.recordModifiedOnWatch
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+    var window: UIWindow? 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
